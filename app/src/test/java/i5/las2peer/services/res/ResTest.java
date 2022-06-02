72bc894a98733efc03a917f81cefa7554714b9dc
PhilCAEUser <mail@rwth>
2022-06-02 10:52:50 +0000

    update2

--- app/src/test/java/i5/las2peer/services/res/ResTest.java
+++ app/src/test/java/i5/las2peer/services/res/ResTest.java
@@ -100,7 +100,7 @@ public class ResTest {
     try {
       ClientResponse result = c.sendRequest("GET", mainPath + "/test", "");
     
-      Assert.assertNotEquals("Status code not equals 201 [401453]", result.getHttpCode(), 201);
+      Assert.assertNotEquals("Status code not equals 200 [695493]", result.getHttpCode(), 200);
 
       System.out.println("Result of 'test$HTTP_Method_Name$': " + result.getResponse().trim());
     } catch (Exception e) {
